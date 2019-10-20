abd0cf89c895e3cab0a1d5e1621c6fb4106ebedd
Rohan Ahuja <rahuja2@umbc.edu>
2019-10-20 15:31:22 -0400

    Updates

---
+++ precache-manifest.edbb73e342ccfed988ecec9268f8c771.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78cd2131c86d07b74edb8ee452046325",
+    "url": "/React-Todo-App/index.html"
+  },
+  {
+    "revision": "9a6b2eefc937885d37d3",
+    "url": "/React-Todo-App/static/css/main.8840973b.chunk.css"
+  },
+  {
+    "revision": "db31081431c0ba74915e",
+    "url": "/React-Todo-App/static/js/2.7ce60389.chunk.js"
+  },
+  {
+    "revision": "9a6b2eefc937885d37d3",
+    "url": "/React-Todo-App/static/js/main.e650c4f7.chunk.js"
+  },
+  {
+    "revision": "49de167db724272f6490",
+    "url": "/React-Todo-App/static/js/runtime-main.737bf6b9.js"
+  }
+]);
